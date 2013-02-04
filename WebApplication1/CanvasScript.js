11efe19949bde680793397d1a87797f61cd1ca30
Phil Standen <philip_standen@hotmail.co.uk>
2013-02-04 17:09:34 +0000

    Initial

---
+++ WebApplication1/CanvasScript.js
@@ -0,0 +1,13 @@
+﻿window.onload = draw;
+
+function draw() {
+    var canvas = document.getElementById("myCanvas");
+    if (canvas.getContext) {
+        var context = canvas.getContext("2d");
+        context.fillStyle = "rgba(0, 0, 255, .3)";
+        context.fillRect(25, 25, 125, 125);
+    }
+    else {
+        // Put code to execute if browser doesn't have canvas support here
+    }
+}
